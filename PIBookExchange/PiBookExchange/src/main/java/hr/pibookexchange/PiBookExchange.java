d380815116b00a549f395064a88d0e6e0dba3770
UnhandledExceptionOnLine1 <bruno.bogdan02@gmail.com>
2024-12-16 22:08:44 +0100

    .

---
+++ PIBookExchange/PiBookExchange/src/main/java/hr/pibookexchange/PiBookExchange.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package hr.pibookexchange;
+
+/**
+ *
+ * @author bruno
+ */
+public class PiBookExchange {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
