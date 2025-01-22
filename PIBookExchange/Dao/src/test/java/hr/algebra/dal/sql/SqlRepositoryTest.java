1bcf153f2e9d402b5ad5548df6894960fd1ed467
UnhandledExceptionOnLine1 <bruno.bogdan02@gmail.com>
2025-01-22 16:17:59 +0100

    testing sqlrepo
    just started...

---
+++ PIBookExchange/Dao/src/test/java/hr/algebra/dal/sql/SqlRepositoryTest.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package hr.algebra.dal.sql;
+
+import hr.algebra.model.User;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author bruno
+ */
+public class SqlRepositoryTest {
+    
+    User user = new User("username", "password", "firstname", "lastname", "address", "telephone", "email", false);
+    
+}
